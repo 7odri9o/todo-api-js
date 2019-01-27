28bbd45c9b885ed94a81460c80acf91dc82ff70c
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2019-01-27 21:01:40 -0200

    Adicionando eslint configs e scripts test

---
+++ test/unit/test.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+describe('Fake Test Suite', function () {
+	describe('Fake Test Suite', function () {
+		it('Fake Test', function () {
+			assert.equal([1, 2, 3].indexOf(4), -1)
+		})
+	})
+})
